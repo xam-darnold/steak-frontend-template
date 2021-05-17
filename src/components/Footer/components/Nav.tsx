e93b84faf1b73bbf7a5f58d319b2d9a3cc336137
xam-darnold <xamcrypto@gmail.com>
2021-05-17 15:01:47 -0500

    Add hover to planets

--- src/components/Footer/components/Nav.tsx
+++ src/components/Footer/components/Nav.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 import footerImage from '../../../assets/background/footer.png'
 import discord from '../../../assets/background/discord_planet.png'
 import steak from '../../../assets/background/steak_planet.png'
@@ -8,27 +9,89 @@ import github from '../../../assets/background/github_planet.png'
 import medium from '../../../assets/background/medium_planet.png'
 import twitter from '../../../assets/background/twitter_planet.png'
 
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] }
+
 const Nav: React.FC = () => {
   return (
     <>
       <StyledNav>
-        <StyledLink target="_blank" href="https://discord.gg/GHJdA56t" style={{top: "300px", left: "650px"}}>
-          <img src={discord} width={100} alt="" />
+        <StyledLink
+          target="_blank"
+          href="https://discord.gg/GHJdA56t"
+          style={{ top: '300px', left: '650px' }}
+        >
+          <motion.img
+            whileHover={{ scale: 1.1 }}
+            transition={transition}
+            src={discord}
+            width={100}
+            alt=""
+          />
         </StyledLink>
-        <StyledLink target="_blank" href="https://twitter.com/stake_steak" style={{top: "100px", right: "50px"}}>
-          <img src={twitter} width={150} alt="" />
+        <StyledLink
+          target="_blank"
+          href="https://twitter.com/stake_steak"
+          style={{ top: '100px', right: '50px' }}
+        >
+          <motion.img
+            whileHover={{ scale: 1.1 }}
+            transition={transition}
+            src={twitter}
+            width={150}
+            alt=""
+          />
         </StyledLink>
-        <StyledLink target="_blank" href="https://twitter.com/stake_steak" style={{top: "40px", right: "400px"}}>
-          <img src={steak} width={200} alt="" />
+        <StyledLink
+          target="_blank"
+          href="https://stakesteak.com"
+          style={{ top: '40px', right: '400px' }}
+        >
+          <motion.img
+            whileHover={{ scale: 1.1 }}
+            transition={transition}
+            src={steak}
+            width={200}
+            alt=""
+          />
         </StyledLink>
-        <StyledLink target="_blank" href="https://twitter.com/stake_steak" style={{top: "250px", left: "350px"}}>
-          <img src={ifusd} width={200} alt="" />
+        <StyledLink
+          target="_blank"
+          href="https://stakesteak.com/staking-fusd"
+          style={{ top: '250px', left: '350px' }}
+        >
+          <motion.img
+            whileHover={{ scale: 1.1 }}
+            transition={transition}
+            src={ifusd}
+            width={200}
+            alt=""
+          />
         </StyledLink>
-        <StyledLink target="_blank" href="https://twitter.com/stake_steak" style={{top: "50px", left: "200px"}}>
-          <img src={github} width={150} alt="" />
+        <StyledLink
+          target="_blank"
+          href="https://twitter.com/stake_steak"
+          style={{ top: '50px', left: '200px' }}
+        >
+          <motion.img
+            whileHover={{ scale: 1.1 }}
+            transition={transition}
+            src={github}
+            width={150}
+            alt=""
+          />
         </StyledLink>
-        <StyledLink target="_blank" href="https://twitter.com/stake_steak" style={{top: "200px", right: "900px"}}>
-          <img src={medium} width={100} alt="" />
+        <StyledLink
+          target="_blank"
+          href="https://stakesteak.medium.com"
+          style={{ top: '200px', right: '900px' }}
+        >
+          <motion.img
+            whileHover={{ scale: 1.1 }}
+            transition={transition}
+            src={medium}
+            width={100}
+            alt=""
+          />
         </StyledLink>
       </StyledNav>
       <StyledImage>
