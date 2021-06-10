042ca0d869f70fe3e27e77c2c517edf3ad9d00f9
annniewang <annaxiami@gmail.com>
2021-06-10 17:49:54 -0500

    added icons

--- src/components/TopBar/components/sidebarData.js
+++ src/components/TopBar/components/sidebarData.js
@@ -1,50 +1,50 @@
-// import React from 'react'
-// import * as FaIcons from 'react-icons/fa'
-// import * as AiIcons from 'react-icons/ai'
-// import * as IoIcons from 'react-icons/io'
-// import * as GiIcons from 'react-icons/gi'
-// import * as RiIcons from 'react-icons/ri'
+import React from 'react'
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+import * as IoIcons from 'react-icons/io'
+import * as GiIcons from 'react-icons/gi'
+import * as RiIcons from 'react-icons/ri'
 
 const SidebarData = [
     {
         title: 'Home',
         path: '/',
-        // icon: <AiIcons.AiFillHome/>,
+        icon: <AiIcons.AiFillHome/>,
         cName: 'nav-text'
     },
 
     {
         title: 'Steakhouse',
         path: '/steakhouse',
-        // icon: <GiIcons.GiKnifeFork/>,
+        icon: <GiIcons.GiKnifeFork/>,
         cName: 'nav-text'
     },
 
     {
         title: 'XSteak',
         path: '/xSteak',
-        // icon: <GiIcons.GiSteak/>,
+        icon: <GiIcons.GiSteak/>,
         cName: 'nav-text'
     },
 
     {
         title: 'iFusd',
         path: '/iFusd',
-        // icon: <RiIcons.RiCoinsLine/>,
+        icon: <RiIcons.RiCoinsLine/>,
         cName: 'nav-text'
     },
 
     {
         title: 'Product Paper',
         path: '/productPaper',
-        // icon: <IoIcons.IoIosPaper/>,
+        icon: <FaIcons.FaNewspaper/>,
         cName: 'nav-text'
     },
 
     {
         title: 'Docs',
         path: '/docs',
-        // icon: <IoIcons.IoDocumentTextOutline/>,
+        icon: <IoIcons.IoIosPaper/>,
         cName: 'nav-text'
     }
 ]
