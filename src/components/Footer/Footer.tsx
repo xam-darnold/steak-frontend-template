7c6ba99080241517038c9719cc248b720c456752
annniewang <annaxiami@gmail.com>
2021-06-15 21:38:06 -0500

    footer change

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,44 +1,58 @@
-import React, {useState, useEffect} from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import { FooterContainer } from './containers/footer'
 
-import Nav from './components/Nav'
+function App() {
+  return(
+    <>
+      <FooterContainer/>
+    </>
+  )
+}
 
-const Footer: React.FC = () => {
-  const [nav, setNav] = useState<Boolean>(false);
-  const showNav = () => {
-    if (window.innerWidth <= 960) {
-      setNav(false);
-    } else {
-      setNav(true);
-    }
-  };
+export default App;
 
-  useEffect(() => {
-    showNav();
-  }, []);
 
-  window.addEventListener("resize", showNav);
-  return (
-  <StyledFooter>
-    <StyledFooterInner>
-    {nav && <Nav />}
-    </StyledFooterInner>
-  </StyledFooter>
-)}
+// import React, {useState, useEffect} from 'react'
+// import styled from 'styled-components'
 
-const StyledFooter = styled.footer`
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
-`
-const StyledFooterInner = styled.div`
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  height: ${props => props.theme.topBarSize}px;
-  max-width: ${props => props.theme.siteWidth}px;
-  width: 100%;
-`
+// import Nav from './components/Nav'
 
-export default Footer
+// const Footer: React.FC = () => {
+//   const [nav, setNav] = useState<Boolean>(false);
+//   const showNav = () => {
+//     if (window.innerWidth <= 960) {
+//       setNav(false);
+//     } else {
+//       setNav(true);
+//     }
+//   };
+
+//   useEffect(() => {
+//     showNav();
+//   }, []);
+
+//   window.addEventListener("resize", showNav);
+//   return (
+//   <StyledFooter>
+//     <StyledFooterInner>
+//     {nav && <Nav />}
+//     </StyledFooterInner>
+//   </StyledFooter>
+// )}
+
+// const StyledFooter = styled.footer`
+//   align-items: center;
+//   display: flex;
+//   justify-content: center;
+//   margin-top: 20px;
+// `
+// const StyledFooterInner = styled.div`
+//   align-items: center;
+//   display: flex;
+//   justify-content: center;
+//   height: ${props => props.theme.topBarSize}px;
+//   max-width: ${props => props.theme.siteWidth}px;
+//   width: 100%;
+// `
+
+// export default Footer
